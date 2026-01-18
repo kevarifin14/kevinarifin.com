5fcebb0a237493892b2b7465c1109511add15910
Kevin Arifin <kevarifin14@gmail.com>
2026-01-18 12:56:53 -0800

    restrict login

--- src/tools/auth/login/tool.ts
+++ src/tools/auth/login/tool.ts
@@ -5,6 +5,8 @@ import { createClient } from "../server";
 export const authLogin = createTool(authLoginSchema, async (params) => {
   const supabase = await createClient();
 
+  if (!["kevarifin14@gmail.com"].includes(params.email)) throw new Error("Unauthorized");
+
   await supabase.auth.signInWithOtp({
     email: params.email,
     options: { shouldCreateUser: true },
